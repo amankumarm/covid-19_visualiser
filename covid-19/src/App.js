916bb5c92a171dbc2047ee0b98c417dc234b4ed2
amankumarm <amankumarm441@gmail.com>
2021-06-27 20:37:32 +0530

    buid

--- covid-19/src/App.js
+++
@@ -1,7 +0,0 @@
-import  Home from "./components/home";
-const App=()=> (
-<div>
-    <Home />
-</div>
-)
-export default App
--- covid-19/src/constants.js
+++
@@ -1,7 +0,0 @@
-const client='localhost:3000'
-const flaskgetall='https://cov-flask.herokuapp.com/'
-const flaskgetstatedaily='https://cov-flask.herokuapp.com/getstatedaily'
-const flaskgetstateall='https://cov-flask.herokuapp.com/getstateall'
-const flaskgetstatehistory='https://cov-flask.herokuapp.com/getstatesdailydata?state='
-
-module.exports={client,flaskgetstatedaily,flaskgetstateall,flaskgetall,flaskgetstatehistory}
