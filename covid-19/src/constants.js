a7d5241ae0a46327d2f6e8301dae7bf911444985
amankumarm <amankumarm441@gmail.com>
2021-06-27 20:31:09 +0530

    deploying frontend

--- covid-19/src/constants.js
+++ covid-19/src/constants.js
@@ -1,7 +1,7 @@
 const client='localhost:3000'
-const flaskgetall='http://127.0.0.1:5000/'
-const flaskgetstatedaily='http://127.0.0.1:5000/getstatedaily'
-const flaskgetstateall='http://127.0.0.1:5000/getstateall'
-const flaskgetstatehistory='http://localhost:5000/getstatesdailydata?state='
+const flaskgetall='https://cov-flask.herokuapp.com/'
+const flaskgetstatedaily='https://cov-flask.herokuapp.com/getstatedaily'
+const flaskgetstateall='https://cov-flask.herokuapp.com/getstateall'
+const flaskgetstatehistory='https://cov-flask.herokuapp.com/getstatesdailydata?state='
 
 module.exports={client,flaskgetstatedaily,flaskgetstateall,flaskgetall,flaskgetstatehistory}
